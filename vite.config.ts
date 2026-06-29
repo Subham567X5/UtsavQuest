d25821009f8acfeba00bd68c9f91c4c284f43c1b
tarzo <tarzo@users.noreply.github.com>
2026-06-29 13:27:56 +0530

    Configure GitHub Pages deployment workflow and set base URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/UtsavQuest/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
